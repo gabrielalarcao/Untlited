fe628062aeda83a555097488da0d6ed90a8db804
Artur Melo Barbosa <arturbarbosati@gmail.com>
2018-12-03 16:45:23 -0200

    Inclusão pasta Models

---
+++ Dhyol/Dhyol/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Dhyol.Models
+{
+    
+    public class User
+    {
+        public int ID { get; }
+        public string Name { get; set; }
+        public string Email { get; private set; }
+        public string Kind { get; set; }
+        private string _password;
+
+        public string Password
+        {
+            get { return _password; }
+            private set { _password = value; }
+        }
+
+
+
+    }
+}
